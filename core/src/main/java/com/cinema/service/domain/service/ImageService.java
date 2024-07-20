825054a98672c55fa55b62b8d4d8c208dc080011
Renato <renaato.d.m@gmail.com>
2024-07-20 17:01:06 -0300

    CA-2 aws s3 as image provider implementation

--- core/src/main/java/com/cinema/service/domain/service/ImageService.java
+++ core/src/main/java/com/cinema/service/domain/service/ImageService.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @ComponentScan(basePackages = {"com.cinema.image.impl"})
 public class ImageService {
+
     private final ImageProvider imageProvider;
 
     public String saveImage(String uploadDirectory, MultipartFile imageFile) throws IOException {
--- simple-storage-image-provider/src/main/java/com/cinema/image/impl/SimpleStorageImageProvider.java
+++ simple-storage-image-provider/src/main/java/com/cinema/image/impl/SimpleStorageImageProvider.java
@@ -1,35 +1,61 @@
 package com.cinema.image.impl;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.cinema.image.ImageProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.UUID;
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 
 @Component
 @ConditionalOnProperty(name = "cinema.image-provider", havingValue = "simple-storage")
+@RequiredArgsConstructor
 public class SimpleStorageImageProvider implements ImageProvider {
+    @Value("${api-accesskey}")
+    private String accessKey;
 
-    final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+    @Value("${api-secretkey}")
+    private String secretKey;
 
+    public final String BUCKET_NAME = "cinema-service-movie-images";
 
+    // save as jfif for some reason, probably because the conversion from multipartfile
     @Override
     public String saveImage(String uploadDirectory, MultipartFile imageFile) throws IOException {
+        AWSCredentials credentials = new BasicAWSCredentials(
+                accessKey,
+                secretKey
+        );
+
+        AmazonS3 s3 = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.US_EAST_2)
+                .build();
+
         try {
-            s3.putObject(bucket_name, key_name, new File(file_path));
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(imageFile.getSize());
+            metadata.setContentType(imageFile.getContentType());
+
+            s3.putObject(BUCKET_NAME, "s3-key", imageFile.getInputStream(), metadata);
         } catch (AmazonServiceException e) {
             System.err.println(e.getErrorMessage());
-            System.exit(1);
+            throw new IOException("Erro ao salvar a imagem no S3", e);
         }
-        return uniqueFileName;
+
+        return "ok";
     }
 
     public byte[] getImage(String imageDirectory, String imageName) throws IOException {
